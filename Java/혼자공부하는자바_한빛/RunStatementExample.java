6117cdf884a0f907e988b5d99df59db6c41afee2
Boram-Git <kimboram910@gmail.com>
2023-02-11 16:48:10 +0900

    자바소스파일 입문

---
+++ Java/혼자공부하는자바_한빛/RunStatementExample.java
@@ -0,0 +1,17 @@
+package sec03.exam02;
+
+/*
+ * 작성자: 김보람
+ * 작성일: 2023-02-11
+ */
+
+public class RunStatementExample {
+// 클래스 선언부 // 클래스 이름
+	public static void main(String[] args) {
+	// 메소드 선언부	   // 메소드 이름()
+		int x = 1; // x변수에 1을 저장 -> 변수 x를 선언하고 1을 저장
+		int y  =2; // y변수에 2를 저장 -> 변수 y를 선언하고 2를 저장
+		int result = x + y; // 변수 result 선언하고 x와 y를 더한 값을 저장
+		System.out.println(result); // result 값을 출력 -> 모니터에 출력하는 메소드 호출
+	}
+}
