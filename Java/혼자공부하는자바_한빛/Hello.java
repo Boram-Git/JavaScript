81f7e49a6efeabb013b12070912754f4c519b670
Boram-Git <kimboram910@gmail.com>
2023-02-11 17:11:05 +0900

    main() 메소드를 자동추가하는 법

--- Java/혼자공부하는자바_한빛/Hello.java
+++ Java/혼자공부하는자바_한빛/Hello.java
@@ -1,5 +1,15 @@
+/* main() 메소드를 자동 추가하는 방법:
+1. [New Java Class] 대화상자에서 [Name]입력란에 클래스 이름인 'Hello'를 입력합니다.
+2. main() 메소드를 자동으로 추가하기 위해 [Public static void main(Sting[]args]를 체그한 후 [Finish]버튼을 클릭합니다.
+*/
+
+
 package sec03.exam01;
 
+/*
+작성자: 김보람
+작성일: 2023-02-11
+ */
 public class Hello {
  // 클래스선언부에 Hello라는 클래스이름
 	public static void main(String[] args) {
