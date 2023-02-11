200d449220c27c4b25b03895e401387e264b4f5b
Boram-Git <kimboram910@gmail.com>
2023-02-11 17:03:29 +0900

    Hello Java 출력

---
+++ Java/혼자공부하는자바_한빛/Hello.java
@@ -0,0 +1,9 @@
+package sec03.exam01;
+
+public class Hello {
+ // 클래스선언부에 Hello라는 클래스이름
+	public static void main(String[] args) {
+ // 메소드 선언부에 main() 메소드 이름
+		System.out.println("Hello, Java"); 
+	} // end of main
+} // end of class
