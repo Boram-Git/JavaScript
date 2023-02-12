2bdd5dc3b24d84d8a90893739505eeece137488c
Boram-Git <kimboram910@gmail.com>
2023-02-13 00:07:22 +0900

    byte 타입 변수에 허용 범위를 초과한 값을 대입했을 때 컴파일 에러

---
+++ Java/혼자공부하는자바_한빛/ByteExample.java
@@ -0,0 +1,24 @@
+package sec02.exam02;
+
+/*
+작성자: 김보람
+작성일: 2023-02-12
+ */
+
+public class ByteExample {
+	public static void main(String[] args) {
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 30;
+		byte var5 = 127;
+		byte var6 = 128;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+	}
+}
