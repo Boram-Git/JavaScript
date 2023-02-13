ff538ebab5ac2444237cd11dd964c4d5b70bfcb6
Boram-Git <kimboram910@gmail.com>
2023-02-13 10:12:35 +0900

    문자열을 변수에 저장하는 법-String 타입

---
+++ Java/혼자공부하는자바_한빛/StringExample.java
@@ -0,0 +1,16 @@
+package sec02.exam05;
+
+/*
+작성자: 김보람
+작성일: 2023-02-13
+ */
+
+public class StringExample {
+	public static void main(String[] args) {
+		String name = "김보람";
+		String job = "데이터분석가";
+		
+		System.out.println("이름: " + name);
+		System.out.println("직업: " + job);
+	}
+}
