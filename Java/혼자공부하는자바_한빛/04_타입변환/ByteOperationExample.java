e4def4f58df9dd5e4a4375a1f26f79e546254f93
Boram-Git <kimboram910@gmail.com>
2023-02-16 09:42:19 +0900

    정수 연산에서 자동 타입 변환/피연산자

--- Java/혼자공부하는자바_한빛/04_타입변환/ByteOperationExample.java
+++ Java/혼자공부하는자바_한빛/04_타입변환/ByteOperationExample.java
@@ -8,11 +8,13 @@ package sec03;
 // 자바는 실행 능력을 향상시키기 위해서 컴파일 단계에서 연산을 수행할 수 있다.
 public class ByteOperationExample {
 	public static void main(String[] args) {
+		
 		byte result1 = 10 + 20;
 		System.out.println(result1);
 		
-		byte x = 10;
-		byte y = 20;
+		// 피연산자: 연산에 참여하는 변수나 상수
+		byte x = 10; // x: 피연산자
+		byte y = 20; // y: 피연산자 
 		int result2 = x + y;
 		System.out.println(result2);
 		// 정수 연산식에서 모든 변수가 int 타입으로 변환되는 것은 아님, 두 피연산자 중 허용 범위가 큰 타입으로 변환되어 연산을 수헹한다.
