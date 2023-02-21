538bbb8dd6fc9aa571aedc92a55e10b108821016
Boram-Git <kimboram910@gmail.com>
2023-02-21 13:40:31 +0900

    문자열 결합 연산자

--- Java/혼자공부하는자바_한빛/07_연산자의종류/StringConcatExample.java
+++ Java/혼자공부하는자바_한빛/07_연산자의종류/StringConcatExample.java
@@ -1,32 +1,24 @@
-package sec03;
+package sec03.exam01;
 
 /*
 작성자: 김보람
-작성일: 2023-02-16
-*/ 
+작성일: 2023-02-21
+ */
 
 public class StringConcatExample {
 
 	public static void main(String[] args) {
+		String str1 = "JDK" + 6.0;
+		String str2 = str1 + " 특징";
 		
-		// 숫자 연산
-		int value = 10 + 2 + 8;
-		System.out.println(value);
-		
-		// 문자열 연산
-		String str1 = 10 + 2 + "8"; // 숫자 + 숫자 + 문자열
 		System.out.println(str1);
+		System.out.println(str2);
 		
-		String str2 = 10 + "2" + 8;
-		System.out.println(str2); // 182 or 1028 따라서 182는 오답
-		
-		String str3 = "10" + 2 + 8;
-		System.out.println(str3); // 1010 or 1028 따라서 1010은 오답
-		
-		/* 순차적으로 +연산을 수행하지 않고 우선 연산하고 싶은 부분이 있다면 해당 부분을 괄호()로 감싸준다.
-		   괄호는 최우선으로 연산을 수행한다. */
-		String str4 = "10" + (4 + 8);
-		System.out.println(str4); // 1010
+		String str3 = "JDK" + 3 + 3.0; // 왼쪽에서 오른쪽으로 차례차례 산술연산
+		String str4 = 3 + 3.0 + "JDK"; // 왼쪽에서 오른쪽으로 차례차례 산술연산
+		System.out.println(str3); // >> JDK33.0
+		System.out.println(str4); // >> 6.0JDK 
+
 	}
 
 }
