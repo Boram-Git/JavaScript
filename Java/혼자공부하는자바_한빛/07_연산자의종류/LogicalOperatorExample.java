ddfdc2ab2365ab37e087d73abce17d06bc9d191a
Boram-Git <kimboram910@gmail.com>
2023-02-21 16:13:32 +0900

    논리 연산자

--- Java/혼자공부하는자바_한빛/07_연산자의종류/LogicalOperatorExample.java
+++ Java/혼자공부하는자바_한빛/07_연산자의종류/LogicalOperatorExample.java
@@ -10,27 +10,27 @@ package sec03.exam01;
 public class LogicalOperatorExample {
 
 	public static void main(String[] args) {
-		int charCode = 'A';
+		int charCode = 'A'; // 유니코드: 65
 		
-		if( (charCode>=65) & (charCode<=90) ) {
+		if( (charCode>=65) & (charCode<=90) ) { // true - true -> 출력
 			System.out.println("대문자군요");
 		}
 			
-		if( (charCode>=97) && (charCode<=122) ) {
+		if( (charCode>=97) && (charCode<=122) ) { // false - false
 			System.out.println("소문자군요");
 		}
 		
-		if( !(charCode<48) && !(charCode>57) ) {
+		if( !(charCode<48) && !(charCode>57) ) { // true - false
 			System.out.println("0~9 숫자군요");
 		}
 		
 		int value = 6;
 		
-		if( (value%2==0) | (value%3==0) ) {
+		if( (value%2==0) | (value%3==0) ) { // true - true -> 출력
 			System.out.println("2 또는 3의 배수군요");
 		}
 			
-		if( (value%2==0) || (value%3==0) ) {
+		if( (value%2==0) || (value%3==0) ) { // true - true -> 출력
 				System.out.println("2 또는 3의 배수군요");
 		}
 
