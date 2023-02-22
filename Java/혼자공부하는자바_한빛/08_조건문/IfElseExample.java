62a2bd09c416f155adca7757f5c961349b2b2edc
Boram-Git <kimboram910@gmail.com>
2023-02-22 17:22:10 +0900

    조건문-if문,else문

---
+++ Java/혼자공부하는자바_한빛/08_조건문/IfElseExample.java
@@ -0,0 +1,26 @@
+package sec03.exam01;
+
+/*
+작성자: 김보람
+작성일: 2023-02-22
+ */
+
+// Line을 잘 보고 써야함.
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+		int score = 85;
+		
+		if(score>=90) {
+			System.out.println("점수가 90보다 큽니다.");
+			System.out.println("등급은 A등급입니다.");
+				
+		} else {
+			System.out.println("점수가 90보다 작습니다.");
+			System.out.println("등급은 B등급입니다.");
+		}
+
+	}
+
+}
