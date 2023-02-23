62d9d1057e91ad858a8601223f847b5bb0b8fe65
Boram-Git <kimboram910@gmail.com>
2023-02-23 17:58:23 +0900

    1부터 10까지 출력

---
+++ Java/혼자공부하는자바_한빛/09_반복문/WhilePrintFrom1To10Example.java
@@ -0,0 +1,25 @@
+package sec03.exam01;
+
+/*
+작성자: 김보람
+작성일: 2023-02-23
+ */
+
+public class WhilePrintFrom1To10Example {
+
+	public static void main(String[] args) {
+//		while(int i; i<=10; ++i;) {
+//			System.out.println(); 왼쪽 코드는 for문일 때 코드형식이랑 비슷한데 아무튼 오답코드임
+//		}
+
+		int i = 1;
+		
+		while(i<=10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		
+	}
+
+}
