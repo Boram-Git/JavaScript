7ceac7471601306f8173cee3aa5c9fbd93f8d2a9
Boram-Git <kimboram910@gmail.com>
2023-02-25 00:07:40 +0900

    new 연산자로 배열 생성

--- Java/혼자공부하는자바_한빛/11_배열/ArrayCreateByNewExample.java
+++ Java/혼자공부하는자바_한빛/11_배열/ArrayCreateByNewExample.java
@@ -1,7 +1,5 @@
 package sec03;
 
-import sec02.exam01.integerLiteralExample;
-
 /*
 작성자: 김보람
 작성일: 2023-02-24
@@ -12,19 +10,19 @@ import sec02.exam01.integerLiteralExample;
 public class ArrayCreateByNewExample {
 
 	public static void main(String[] args) {
-		int[] arr1 = new int[3];	// new 연산자로 배열 생성 -> 타입[] 변수 = new 타입[길이];에서 길이는 배열이 저장할 수 있는 값의 개수
+		int[] arr1 = new int[3]; // new 연산자로 배열 생성 -> 타입[] 변수 = new 타입[길이];에서 길이는 배열이 저장할 수 있는 값의 개수
 		for (int i = 0; i < 3; i++) {
 			System.out.println("arr1[ " + i + "] : " + arr1[i]);
 		}
 
-		arr1[0] = 10;	// 배열이 생성되고 나서 특정 인덱스 위치에 새로운 값을 저장하려면 대입 연산자를 사용하면 된다.
-		arr1[1] = 20;	// 변수[인덱스] = 값;에서 배열 arr1의 0, 1, 2 인덱스에 각각 10, 20, 30를 저장하는 코드 
+		arr1[0] = 10; // 배열이 생성되고 나서 특정 인덱스 위치에 새로운 값을 저장하려면 대입 연산자를 사용하면 된다.
+		arr1[1] = 20; // 변수[인덱스] = 값;에서 배열 arr1의 0, 1, 2 인덱스에 각각 10, 20, 30를 저장하는 코드
 		arr1[2] = 30;
 		for (int i = 0; i < 3; i++) {
 			System.out.println("arr1[" + i + "] : " + arr1[i]);
 		}
 
-		double[] arr2 = new double[3];	// new 연산자로 배열 생성
+		double[] arr2 = new double[3]; // new 연산자로 배열 생성
 		for (int i = 0; i < 3; i++) {
 			System.out.println("arr2[" + i + "] : " + arr2[i]);
 		}
@@ -36,7 +34,7 @@ public class ArrayCreateByNewExample {
 			System.out.println("arr2[" + i + "] : " + arr2[i]);
 		}
 
-		String[] arr3 = new String[3];	// new 연산자로 배열 생성
+		String[] arr3 = new String[3]; // new 연산자로 배열 생성
 		for (int i = 0; i < 3; i++) {
 			System.out.println("arr3[" + i + "] : " + arr3[i]);
 		}
